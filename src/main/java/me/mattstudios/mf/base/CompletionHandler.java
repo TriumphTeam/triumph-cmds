e6cf77fcfa469cd1f9c9078af23eb9ba5d5febe1
Matt <xpsykogfx@gmail.com>
2019-10-02 19:44:17 +0100

    Copyright testing.

--- src/main/java/me/mattstudios/mf/base/CompletionHandler.java
+++ src/main/java/me/mattstudios/mf/base/CompletionHandler.java
@@ -1,3 +1,27 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019 Matt
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package me.mattstudios.mf.base;
 
 import me.mattstudios.mf.base.components.CompletionResolver;
@@ -5,10 +29,15 @@ import me.mattstudios.mf.exceptions.InvalidCompletionIdException;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+@SuppressWarnings("WeakerAccess")
 public class CompletionHandler {
 
     private final Map<String, CompletionResolver> registeredCompletions = new HashMap<>();
@@ -81,6 +110,7 @@ public class CompletionHandler {
      * @param id The ID to check.
      * @return The result of being registered or not.
      */
+    @SuppressWarnings("BooleanMethodIsAlwaysInverted")
     boolean isRegistered(String id) {
         if (id.contains(":")) {
             String[] content = id.split(":");
