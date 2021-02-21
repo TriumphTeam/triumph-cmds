ba514ea1f91b54b6e552c61e36ada66280040d9b
Matt <xpsykogfx@gmail.com>
2021-02-21 05:31:05 +0000

    Moved to gradle and started over

--- core/src/main/java/me/mattstudios/mfcmd/base/components/MfCommand.java
+++
@@ -1,3 +0,0 @@
-package me.mattstudios.mfcmd.base.components;
-
-public interface MfCommand {}
--- core/src/main/java/me/mattstudios/mfcmd/base/components/util/Constant.java
+++
@@ -1,9 +0,0 @@
-package me.mattstudios.mfcmd.base.components.util;
-
-public final class Constant {
-
-    private Constant(){}
-
-    public static final String DEFAULT = "mf-default";
-
-}
