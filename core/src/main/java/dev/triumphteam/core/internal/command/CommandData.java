3b6a52e7cb21d95c83f8f5f9ef4970d770ab4368
Matt <xpsykogfx@gmail.com>
2021-03-05 23:25:19 +0000

    Added command data to abstract command class scanning

--- core/src/main/java/dev/triumphteam/core/internal/command/CommandData.java
+++ core/src/main/java/dev/triumphteam/core/internal/command/CommandData.java
@@ -1,4 +1,82 @@
 package dev.triumphteam.core.internal.command;
 
+import dev.triumphteam.core.annotations.Command;
+import dev.triumphteam.core.exceptions.CommandRegistrationException;
+import dev.triumphteam.core.internal.CommandBase;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.lang.annotation.Annotation;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 public final class CommandData {
+
+    private final String commandName;
+    private final List<String> aliases;
+
+    public CommandData(
+            @NotNull final String commandName,
+            @NotNull final List<String> aliases
+    ) {
+        this.commandName = commandName;
+        this.aliases = aliases;
+    }
+
+    /**
+     * Extracts all the important information from the command class
+     *
+     * @param commandClass The {@link CommandBase} class
+     * @return A new {@link CommandData} with the extracted information
+     */
+    @NotNull
+    public static CommandData from(@NotNull final Class<? extends CommandBase> commandClass) {
+        final List<String> commandNames = extractCommandNames(commandClass);
+        final String commandName = commandNames.get(0);
+        commandNames.remove(0);
+
+        return new CommandData(commandName, commandNames);
+    }
+
+    public String getCommandName() {
+        return commandName;
+    }
+
+    public List<String> getAliases() {
+        return aliases;
+    }
+
+    @NotNull
+    private static List<String> extractCommandNames(@NotNull final Class<? extends CommandBase> commandClass) throws CommandRegistrationException {
+        final Command commandAnnotation = getAnnotationValue(commandClass, Command.class);
+        if (commandAnnotation == null) {
+            throw new CommandRegistrationException("`@Command` annotation is missing in class: " + commandClass.getName() + "!");
+        }
+
+        final List<String> commands = new LinkedList<>(Arrays.asList(commandAnnotation.value()));
+        if (commands.isEmpty()) {
+            throw new CommandRegistrationException("`@Command` annotation is empty in class " + commandClass.getName() + "!");
+        }
+
+        return commands;
+    }
+
+    /**
+     * Util for getting the annotation or null so it doesn't throw exception
+     *
+     * @param commandClass The command class to get the annotation from
+     * @param annotation   The annotation class
+     * @param <T>          Generic type of the annotation
+     * @return The annotation to use
+     */
+    @Nullable
+    private static <T extends Annotation> T getAnnotationValue(
+            @NotNull final Class<? extends CommandBase> commandClass,
+            @NotNull final Class<T> annotation
+    ) {
+        if (!commandClass.isAnnotationPresent(annotation)) return null;
+        return commandClass.getAnnotation(annotation);
+    }
+
 }
--- core/src/test/java/dev/triumphteam/core/test/objects/TestCommandManager.kt
+++ core/src/test/java/dev/triumphteam/core/test/objects/TestCommandManager.kt
@@ -2,7 +2,7 @@ package dev.triumphteam.core.test.objects
 
 import dev.triumphteam.core.internal.CommandBase
 import dev.triumphteam.core.internal.CommandManager
-import dev.triumphteam.core.internal.utils.AnnotationUtils
+import dev.triumphteam.core.internal.command.CommandData
 
 
 class TestCommandManager : CommandManager() {
@@ -10,10 +10,11 @@ class TestCommandManager : CommandManager() {
     val commands = mutableMapOf<String, TestCommandHandler>()
 
     override fun registerCommand(command: CommandBase) {
-        val commandClass: Class<out CommandBase> = command.javaClass
+        val commandClass = command.javaClass
+        val data = CommandData.from(commandClass)
 
-        val aliases = AnnotationUtils.extractAliases(commandClass)
-
+        println(data.commandName)
+        println(data.aliases)
     }
 
 }
