2eac6a488721aa25e5fc5dbde68da5cbfcf3ad01
Matt <xpsykogfx@gmail.com>
2021-06-17 04:40:54 +0100

    Added all the basic argument types

--- core/src/main/java/dev/triumphteam/core/registry/ArgumentRegistry.java
+++ core/src/main/java/dev/triumphteam/core/registry/ArgumentRegistry.java
@@ -1,6 +1,9 @@
 package dev.triumphteam.core.registry;
 
+import com.google.common.primitives.Doubles;
+import com.google.common.primitives.Floats;
 import com.google.common.primitives.Ints;
+import com.google.common.primitives.Longs;
 import dev.triumphteam.core.argument.ArgumentResolver;
 import dev.triumphteam.core.exceptions.SubCommandRegistrationException;
 import org.jetbrains.annotations.NotNull;
@@ -12,10 +15,30 @@ public final class ArgumentRegistry {
 
     private final Map<Class<?>, ArgumentResolver> registeredArguments = new HashMap<>();
 
+    @SuppressWarnings("UnstableApiUsage")
     public ArgumentRegistry() {
         register(int.class, arg -> Ints.tryParse(String.valueOf(arg)));
         register(Integer.class, arg -> Ints.tryParse(String.valueOf(arg)));
+
+        register(long.class, arg -> Longs.tryParse(String.valueOf(arg)));
+        register(Long.class, arg -> Longs.tryParse(String.valueOf(arg)));
+
+        register(float.class, arg -> Floats.tryParse(String.valueOf(arg)));
+        register(Float.class, arg -> Floats.tryParse(String.valueOf(arg)));
+
+        register(double.class, arg -> Doubles.tryParse(String.valueOf(arg)));
+        register(Double.class, arg -> Doubles.tryParse(String.valueOf(arg)));
+
+        register(String[].class, arg -> {
+            if (arg instanceof String[]) return arg;
+            return null;
+        });
+
+        register(Boolean.class, arg -> Boolean.valueOf(String.valueOf(arg)));
+        register(boolean.class, arg -> Boolean.valueOf(String.valueOf(arg)));
+
         register(String.class, String::valueOf);
+
     }
 
     public void register(@NotNull final Class<?> clazz, final ArgumentResolver argument) {
