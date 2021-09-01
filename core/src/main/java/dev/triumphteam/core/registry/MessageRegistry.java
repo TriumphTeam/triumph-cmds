ae3e2f4b2e66664126fc76063393a589524a5c6c
Matt <xpsykogfx@gmail.com>
2021-09-01 16:38:19 +0100

    Added message registry and resolvers

---
+++ core/src/main/java/dev/triumphteam/core/registry/MessageRegistry.java
@@ -0,0 +1,23 @@
+package dev.triumphteam.core.registry;
+
+import dev.triumphteam.core.command.message.MessageResolver;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public final class MessageRegistry<S> {
+
+    private final Map<String, MessageResolver<S>> messages = new HashMap<>();
+
+    public void register(@NotNull final String key, @NotNull final MessageResolver<S> resolver) {
+        messages.put(key, resolver);
+    }
+
+    public void sendMessage(@NotNull final String key, @NotNull final S sender) {
+        final MessageResolver<S> messageResolver = messages.get(key);
+        if (messageResolver == null) return;
+        messageResolver.resolve(sender);
+    }
+
+}
