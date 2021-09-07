702514dc7841b5149733116d426311bb4f1503c7
Matt <xpsykogfx@gmail.com>
2021-09-08 00:03:56 +0100

    Updated license

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,6 +38,8 @@ subprojects {
         header = rootProject.file("LICENSE")
         encoding = "UTF-8"
         mapping("kotlin", "JAVADOC_STYLE")
+        mapping("java", "JAVADOC_STYLE")
+
 
         include("**/*.kt")
         include("**/*.java")
