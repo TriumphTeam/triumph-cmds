22322b261c283492ddbc8359a93cdaf3b155ade4
DrZoddiak <JesseMcKee97@gmail.com>
2022-05-13 11:40:30 -0500

    Commit build file

---
+++ minecraft/sponge/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("cmds.base-conventions")
+    id("cmds.library-conventions")
+}
+
+repositories {
+    maven {
+        name = "Sponge"
+        url = uri("https://repo.spongepowered.org/maven")
+    }
+}
+
+dependencies {
+    api(project(":triumph-cmd-core"))
+    compileOnly("org.spongepowered:spongeapi:8.0.0")
+}
